33edd8999671a6a9b5c67e22d35f32487272244e
Diego <sheremetyevo2@hotmail.com>
2021-12-19 22:40:59 -0700

    First commit

---
+++ src/servidor/PruebaServidor.java
@@ -0,0 +1,14 @@
+
+package servidor;
+
+import javax.swing.JFrame;
+
+public class PruebaServidor {
+    public static void main(String[] args) {
+        
+        Servidor aplicacion = new Servidor();
+        aplicacion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        aplicacion.ejecutarServidor();
+    }
+}
+
